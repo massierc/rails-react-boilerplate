f669cefe3ace8173ec0c029338a0c29aac1fd07a
massierc <massimo.ercolani28@gmail.com>
2019-02-24 20:47:36 +0100

    add user authentication to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   prepend_view_path Rails.root.join("frontend")
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 end
