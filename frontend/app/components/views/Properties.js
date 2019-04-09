e19b60e9b720cd452b0b4dcc371f338278a72a26
massierc <massimo.ercolani28@gmail.com>
2019-04-09 14:38:06 +0200

    add PropTypes

--- frontend/app/components/views/Properties.js
+++ frontend/app/components/views/Properties.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { List } from 'immutable';
 import { propertiesActions } from '../../store/actions';
 
 function Properties({ fetchProperties, properties }) {
@@ -7,13 +9,18 @@ function Properties({ fetchProperties, properties }) {
 
   return (
     <div>
-      <p>Properties (react)</p>
+      <h1>Properties (react)</h1>
       <ul>
         {properties.map(prop => <li key={prop.get('id')}>{prop.get('name')}</li>)}
       </ul>
     </div>
   );
 }
+
+Properties.propTypes = {
+  fetchProperties: PropTypes.func.isRequired,
+  properties: PropTypes.instanceOf(List).isRequired,
+};
 
 const mapStateToProps = state => ({
   properties: state.getIn(['propertiesReducer', 'properties']),
